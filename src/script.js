b5f6aa6e6f9f1048562765e623c6bc187ad42884
chardi90 <chardi90@hotmail.com>
2025-05-02 11:57:24 +0100

    Create basic HTML, JS and CSS files
    Basic content and file structure created.

---
+++ src/script.js
@@ -0,0 +1,36 @@
+let weather = [
+  {
+    name: "Paris",
+    temp: 19.7,
+    faren: 66,
+    humidity: 80,
+  },
+  {
+    name: "Tokyo",
+    temp: 17.3,
+    faren: 10,
+    humidity: 50,
+  },
+  {
+    name: "Lisbon",
+    temp: 30.2,
+    faren: 17,
+    humidity: 20,
+  },
+  {
+    name: "San Francisco",
+    temp: 20.9,
+    faren: 3,
+    humidity: 100,
+  },
+  {
+    name: "Oslo",
+    temp: -5,
+    faren: 2,
+    humidity: 20,
+  },
+];
+
+alert(
+  `It is currently ${city.temp}°C (${city.faren}°F) in ${city} with a humidity of ${city.humidity}%`
+);
